5d34c3218aee57910440ac5032b26b814e4318de
Phi Nguyen <naynhokia.anhyeuem@gmail.com>
2015-05-29 22:46:40 +0700

    - Added: Preview screen

---
+++ Bricks Animated 3D/Bricks Animated 3D/PreviewLegoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PreviewLegoViewController.h
+//  Bricks Animated 3D
+//
+//  Created by Phi Nguyen on 5/29/15.
+//  Copyright (c) 2015 Thien Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreviewLegoViewController : UIViewController
+
+@end
---
+++ Bricks Animated 3D/Bricks Animated 3D/PreviewLegoViewController.m
@@ -0,0 +1,21 @@
+//
+//  PreviewLegoViewController.m
+//  Bricks Animated 3D
+//
+//  Created by Phi Nguyen on 5/29/15.
+//  Copyright (c) 2015 Thien Nguyen. All rights reserved.
+//
+
+#import "PreviewLegoViewController.h"
+
+@interface PreviewLegoViewController ()
+
+@end
+
+@implementation PreviewLegoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+@end
